1e7a947d8b3a1614656f4a3c70fca4a944267394
mahi4905 <mahendrapk2005@gmail.com>
2026-02-01 21:36:47 +0530

    error handling

--- src/index.js
+++ src/index.js
@@ -4,8 +4,18 @@ dotenv.config({ path: './.env' })
 
 
 import connectToDatabase from "./db/index.js";
+import { app } from "./app.js";
 
-connectToDatabase();
+connectToDatabase().then(() => {
+    app.listen(process.env.PORT||8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+}).catch((error) => {
+    console.error("Failed to start the server:", error);
+    
+});
+
+
 
 
 
